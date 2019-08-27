726c9ddc31fb28f6e15a09add4389a9c2b99074c
David De Anda <omarandstuff@gmail.com>
2019-08-27 11:43:54 -0500

    Basic endpoint

---
+++ index.js
@@ -0,0 +1,25 @@
+var app = require('express')()
+var http = require('http').createServer(app)
+var io = require('socket.io')(http)
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/endpoint', function(req, res) {
+  io.emit('chat message', JSON.stringify(req.body))
+  res.send()
+})
+
+io.on('connection', function(socket) {
+  socket.on('chat message', function(msg) {
+    io.emit('chat message', msg)
+  })
+})
+
+http.listen(3000, function() {
+  console.log('listening on *:3000')
+})
